3b7643f0a076e59b60c0cf169e374561ece3cbb6
Ty Everton <everton_915@yahoo.com>
2021-09-05 17:34:51 -0600

    building out boiler plates

---
+++ src/Components/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react"
+import NavBar from "./NavBar"
+import "./homepage.css"
+
+const HomePage = () => {
+  return (
+    <div>
+      {/* <h1 className="intro">
+        Hello, I am Ty, and I am a web developer.
+      </h1> */}
+      <NavBar />
+    </div>
+  )
+}
+
+
+export default HomePage
---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./navbar.css"
+
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <button>PROJECTS</button>
+    </div>
+  )
+}
+
+export default NavBar
