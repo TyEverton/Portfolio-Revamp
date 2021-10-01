54267489cc3c18ec737b0c6dbd672897a3f47645
Ty Everton <everton_915@yahoo.com>
2021-10-01 15:55:59 -0600

    working on project section styling

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,29 +1,38 @@
-import React from "react"
-import MealItYourWay from "../MealItYourWay.jpg"
-import "./projects.css"
+import React from 'react'
+import MealItYourWay from '../MealItYourWay.jpg'
+import './projects.css'
 const Projects = () => {
   return (
     <div>
       <h1 className="projectsHeader">Projects</h1>
       <div className="projectContainer">
-        <img
-          alt="MealItYourWay Snapshot"
-          className="mealProject"
-          src={MealItYourWay}
-        ></img>
         <section className="mealProjectBody">
+          <img
+            alt="MealItYourWay Snapshot"
+            className="mealPhoto"
+            src={MealItYourWay}
+          ></img>
           <h3 className="mealProjectHeader">Meal It, Your Way</h3>
-          <p>Techologies Used: Figma</p>
-          <p>
-            Technologies to be Used: React, Google Places API, PostgreSQL,
-            Massive
-          </p>
-          <p>
-            I had a blast designing the UX and UI of Meal It Your Way. While it
-            is still in its design phase, this app is going to be an alternative
-            to Uber Eats and Door Dash. Mostly to challenge myself and to build
-            something I can be proud of.
-          </p>
+          <section className="mealItSummary">
+            <p>Techologies Used: Figma</p>
+            <p>
+              Technologies to be Used: React, Google Places API, PostgreSQL,
+              Massive
+            </p>
+            <p>
+              I had a blast designing the UX and UI of Meal It Your Way. While
+              it is still in its design phase, this app is going to be an
+              alternative to apps like Uber Eats and Door Dash. Mostly to
+              challenge myself and to build something I can be proud of.
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/TyEverton/Meal_It_Your_Way"
+            >
+              Github
+            </a>
+          </section>
         </section>
       </div>
     </div>
